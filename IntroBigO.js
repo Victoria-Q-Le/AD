2d43b5b80d5bad482fe08ff3f7ab5bcd6738defe
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-06-29 13:10:27 -0400

    example for timing our code

--- IntroBigO.js
+++ IntroBigO.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+// Write a function to calculate the sum from 1 up to (and including n) some number
+
+// function addUpto1 (n) {
+//   let total = 0;
+//   for (let i = 1; i <= n; i++){
+//     total += i
+//   }
+//   console.log(total);
+//   return total
+// }
+//
+// addUpto1 (5)
+
+function addUpto2 (n) {
+  let total = n * (n+1)/2
+  console.log(total);
+  return total
+}
+
+addUpto2 (5)
